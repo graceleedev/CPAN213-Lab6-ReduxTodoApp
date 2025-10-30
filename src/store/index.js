5406d107e7f02de1ddd41afe0e38c3d4918c9813
Grace <gracelee.dev@gmail.com>
2025-10-29 21:51:49 -0400

    set up Redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { devToolsEnhancer } from '@redux-devtools/extension';
 import todosReducer from './todosSlice';
+
 export const store = configureStore({
   reducer: {
     todos: todosReducer,
+    enhancers: [devToolsEnhancer()],
   },
 });
